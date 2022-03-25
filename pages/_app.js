55b6ffae4bcc412c6a121d1ea3fdd302fcf59b61
NIRUPASHREE <51870577+NIRUPASHREE@users.noreply.github.com>
2022-03-25 14:46:24 +0530

    line.init added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useEffect } from 'react';
+
+const liffId = process.env.NEXT_PUBLIC_LIFF_ID;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    async function liffLogin() {
+      const liff = (await import('@line/liff')).default;
+      try {
+        await liff.init({ liffId });
+      } catch (err) {
+        console.error('liff init error', err.message);
+      }
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      }
+    }
+    liffLogin();
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
